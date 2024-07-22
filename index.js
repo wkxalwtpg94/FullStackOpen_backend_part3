cef4f8556067b6242a2d478e5638ccbb9faf81cb
Ken <kenxhen@gmail.com>
2024-07-22 15:55:17 +0800

    Add frontend to node express via deployable mode dist

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var morgan = require("morgan")
 const cors = require("cors")
 
 app.use(cors())
-
 app.use(express.json())
+app.use(express.static("dist"))
 
 morgan.token("data", function (req, res) {
     const sliced = Object.fromEntries(
