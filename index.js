aca31875b6b81c433149bfedd4063ab145bea865
Ken <kenxhen@gmail.com>
2024-07-29 01:36:53 +0800

    Add functionality for GET-ing individual person based on ID

--- index.js
+++ index.js
@@ -65,14 +65,9 @@ app.get("/api/persons", (request, response) => {
 })
 
 app.get(`/api/persons/:id`, (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })  
 })
 
 app.post("/api/persons", (request, response) => {
