e1208a914d0d5743cce87531654e35d628f635dc
Ken <kenxhen@gmail.com>
2024-08-20 15:58:11 +0800

    Expand validation for name to be at least 3 characters long and expand frotnend to display error message when validation error occurs

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get(`/api/persons/:id`, (request, response) => {
     .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
     
     if (body.name === undefined) {
@@ -110,9 +110,11 @@ app.post("/api/persons", (request, response) => {
                     number: body.number,
                   })
 
-                  person.save().then(savedPerson => {
+                  person.save()
+                  .then(savedPerson => {
                     response.json(savedPerson)
                   })
+                  .catch(error => next(error))
                 }
               })
     }    
@@ -147,6 +149,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id"})
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
   
   next(error)
